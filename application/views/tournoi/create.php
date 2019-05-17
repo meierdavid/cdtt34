85338dfc744015cc2e5e4b131ab4a9c071930c44
meierdavid <davidmeier@hotmail.fr>
2019-05-17 23:05:23 +0200

    debug

--- application/views/tournoi/create.php
+++ application/views/tournoi/create.php
@@ -1,18 +1,21 @@
 
 
 <div class="container login-container2" align="left">
-    <div class="row">
-        <div class="notification is-danger">
-            <?php echo validation_errors(); ?> 
-        </div>
-        <?php echo form_open('tournoi/create','class="m-auto"'); ?>
-        <div class="form-group">
-            <label for="create_nom_tournoi" class="control-label">Nom du tournoi</label>
-            <input id="create_nom_tournoi" type="text" class="form-control" name="nomTournoi"  value="" size="30" required /> 
-        </div>
+    <div style=" margin-top: 20%">
+        <div class="row">
+            <div class="notification is-danger">
+                <?php echo validation_errors(); ?> 
+            </div>
+            <?php echo form_open('tournoi/create', 'class="m-auto"'); ?>
+            <h2>Créer un Tournoi</h2>
+            <div class="form-group">
+                <label for="create_nom_tournoi" class="control-label">Nom du tournoi</label>
+                <input id="create_nom_tournoi" type="text" class="form-control" name="nomTournoi"  value="" size="30" required /> 
+            </div>
 
-        <div class="text-center"><input class="btn-primary" type="submit" value="créer" /></div>
+            <div class="text-center"><input class="btn-primary" type="submit" value="créer" /></div>
 
-        </form>  
+            </form>  
+        </div>
     </div>
 </div>
