fcf203de3f39980acf7e591872b27d0e0e47a80b
meierdavid <davidmeier@hotmail.fr>
2019-05-17 16:02:45 +0200

    debug

--- application/views/departement/create.php
+++ application/views/departement/create.php
@@ -3,17 +3,17 @@
 <div class="container" >
     <div style=" margin-top: 20%">
     <div class="row">
-        <div class="notification is-danger m-auto">
+        <div class="notification is-danger">
             <?php echo validation_errors(); ?> 
         </div> 
         <?php echo form_open("departement/create"); ?>
-        <div class="form-group m-auto">
+        <div class="form-group">
             <label for="create_nom_departement" class="control-label">Nom du departement</label>
             <input id="create_nom_departement" type="text" class="form-control" name="nomDepartement"  value="" size="30" required /> 
             <label for="create_num_departement" class="control-label">Numéro du Departement</label>
             <input id="create_num_departement" type="number" class="form-control" name="numeroDepartement" value="" size="30" required />          
         </div>
-        <div class="text-centermt-5"><input class="btn-primary" type="submit" value="créer" /></div>
+        <div class=" mt-5"><input class="btn-primary" type="submit" value="créer" /></div>
 
         </form>  
     </div>
