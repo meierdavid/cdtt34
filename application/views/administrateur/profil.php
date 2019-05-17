71ed3e848fb7099f7e49c87b1a8b1fcdad2a6cea
meierdavid <davidmeier@hotmail.fr>
2019-05-17 13:34:48 +0200

    debug

--- application/views/administrateur/profil.php
+++ application/views/administrateur/profil.php
@@ -1,16 +1,15 @@
 <?php if ($isAdmin) { ?>
 <main>
     <div class="container" style=" margin-top: 20%">
-        <div class="row">
-            <div class=" m-auto  mt-5 mb-5 text-center"  >
+        
+            <div class="row m-auto  mt-5 mb-5 text-center"  >
                 <div class=" mr-5 ml-5 ">
                     <p> <?php echo "Votre adresse mail : " . $administrateur[0]->mail ?> </p>
                 </div>
             </div> 
-            <div class=" m-auto ">
+            <div class=" row m-auto ">
                 <a class="btn btn-primary ml-5" href="<?php echo base_url('administrateur/updateMdp'); ?>" role="button">Modifier votre mot de passe</a>
             </div>
-        </div>
     </div>
 </main>
 <?php } ?>
