ace6c8a1b5af29bffa4a348e36cd90ad4391efe9
meierdavid <davidmeier@hotmail.fr>
2019-05-17 13:30:52 +0200

    design profil admin with bootstrap

--- application/views/administrateur/profil.php
+++ application/views/administrateur/profil.php
@@ -2,12 +2,12 @@
 
     <div class="container">
         <div class="row">
-            <div class=" offset-4  mt-5 mb-5 text-center" >
+            <div class=" m-auto  mt-5 mb-5 text-center" >
                 <div class=" mr-5 ml-5 ">
                     <p> <?php echo "Votre adresse mail : " . $administrateur[0]->mail ?> </p>
                 </div>
             </div> 
-            <div class="offset-4 mb-3 ">
+            <div class=" m-auto ">
                 <a class="btn btn-primary ml-5" href="<?php echo base_url('administrateur/updateMdp'); ?>" role="button">Modifier votre mot de passe</a>
             </div>
         </div>
