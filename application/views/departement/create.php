89d69b894db74504195227d66d12f173d88fc432
meierdavid <davidmeier@hotmail.fr>
2019-05-17 15:52:08 +0200

    debug

--- application/views/departement/create.php
+++ application/views/departement/create.php
@@ -10,7 +10,7 @@
             <label for="create_nom_departement" class="control-label">Nom du departement</label>
             <input id="create_nom_departement" type="text" class="form-control" name="nomDepartement"  value="" size="30" required /> 
             <label for="create_num_departement" class="control-label">Numéro du Departement</label>
-            <input id="create_num_departement" type="text" class="form-control" name="numeroDepartement" value="<?php echo $departement[0]->numeroDepartement ?>" size="30" required />          
+            <input id="create_num_departement" type="text" class="form-control" name="numeroDepartement" value="<?php echo $departement[0]->numeroDepartement; ?>" size="30" required />          
         </div>
         <div class="text-center"><input class="btn-primary" type="submit" value="créer" /></div>
 
