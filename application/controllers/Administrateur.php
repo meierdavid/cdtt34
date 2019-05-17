0d25805875afb12273a284a2b3cc81fefbf73be9
meierdavid <davidmeier@hotmail.fr>
2019-05-18 00:54:13 +0200

    debug

--- application/controllers/Administrateur.php
+++ application/controllers/Administrateur.php
@@ -136,7 +136,7 @@ class Administrateur extends Administrator {
     public function deconnexion() {
         $cookieid = parent::getCookieIdName();
         $cookieToken = parent::getCookieTokenName();
-        $this->admin->update(["mail" => $this->encryption->decode($cookieid)],["token" => NULL]); 
+        $this->admin_model->update(["mail" => $this->encryption->decode($cookieid)],["token" => NULL]); 
         delete_cookie($cookieid);
         delete_cookie($cookieToken);     
         $data['isAdmin'] = parent::isAdmin();
