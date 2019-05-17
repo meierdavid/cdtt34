c05b11c7b2c4e6e4c1bd43015fe4d843e38606bb
meierdavid <davidmeier@hotmail.fr>
2019-05-17 15:58:30 +0200

    debug

--- application/views/departement/create.php
+++ application/views/departement/create.php
@@ -19,13 +19,3 @@
     </div>
     </div>
 </div>
-<main class="container">
-        <div style=" margin-top: 20%">
-            <div class="row mb-5">
-                <p class=" m-auto"> <?php echo "Votre adresse mail : " . $administrateur[0]->mail ?> </p>
-            </div> 
-            <div class=" row mt-5">
-                <a class="m-auto btn btn-primary" href="<?php echo base_url('administrateur/updateMdp'); ?>" role="button">Modifier votre mot de passe</a>
-            </div>
-        </div>
-</main>
