d57528db489b93d94bb28bb6e5f8c5f1be2602cf
meierdavid <davidmeier@hotmail.fr>
2019-05-18 15:02:41 +0200

    front bootstrap

--- application/views/administrateur/connexion.php
+++ application/views/administrateur/connexion.php
@@ -1,10 +1,11 @@
 <div class="row justify-content-md-center mt-5 mb-5">
 
     <div class="text-center">
-        <p>Cette page de connexion est dédiée aux responsables départementaux</p>
+
+        <?php echo form_open("welcome/connexion",'class="m-auto"'); ?>
+        <p class="text-warning">Cette page de connexion est dédiée aux responsables départementaux</p>
 
         <h1 class="h3 mb-3 font-weight-normal">Se connecter</h1>
-        <?php echo form_open("welcome/connexion"); ?>
         <label for="inputEmail" class="sr-only">Email address</label>
 
         <input type="email" id="inputEmail" class="form-control mb-1" name="identifiant" value=""  placeholder="Email address" required autofocus />
