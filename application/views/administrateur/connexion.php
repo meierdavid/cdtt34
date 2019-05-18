6ec49a68cbc127ce5def267c9b12a181cefd6fef
meierdavid <davidmeier@hotmail.fr>
2019-05-18 14:34:43 +0200

    debug css

--- application/views/administrateur/connexion.php
+++ application/views/administrateur/connexion.php
@@ -1,9 +1,8 @@
 <div class="row justify-content-md-center mt-5 mb-5">
-    
-        <div class="text-center">
+
+    <div class="text-center">
         <p>Cette page de connexion est dédiée aux responsables départementaux</p>
-        </div>
-        <div class="text-center">
+
         <h1 class="h3 mb-3 font-weight-normal">Se connecter</h1>
         <?php echo form_open("welcome/connexion"); ?>
         <label for="inputEmail" class="sr-only">Email address</label>
@@ -15,7 +14,9 @@
 
         <input class="btn btn-lg btn-primary btn-block" type="submit" name="login" value="Connexion">
         </form>
-        </div>
+        
+    </div>
+    
         
         
 
--- application/views/administrateur/create.php
+++ application/views/administrateur/create.php
@@ -2,10 +2,12 @@
 <div class="row justify-content-md-center mt-5 mb-5">
 
     <div class="text-center">
-        <h1 class="mb-3 font-weight-normal">Ajouter un Administrateur</h1>
+        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
         <?php echo form_open("administrateur/create"); ?>
         <label for="create_email" class="sr-only">Email address</label>
+
         <input type="email" id="create_email" class="form-control mb-1" name="mail" value=""  placeholder="Email address" required autofocus />
+
         <label for="create_password" class="sr-only">Password</label>
         <input id="create_password" class="form-control mb-1" type="password" name="newPassword" value="" placeholder="Password" required />
         <div class="error notification text-danger is-danger"><?php echo form_error('newPassword'); ?> </div>
