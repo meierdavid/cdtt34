8eedbb5c781733d1d573ffb141b60fdccefea5e0
meierdavid <davidmeier@hotmail.fr>
2019-05-18 14:37:22 +0200

    front bootstrap

--- application/views/administrateur/connexion.php
+++ application/views/administrateur/connexion.php
@@ -1,23 +1,24 @@
 <div class="row justify-content-md-center mt-5 mb-5">
 
     <div class="text-center">
-        <p>Cette page de connexion est dédiée aux responsables départementaux</p>
-
+        <p>Cette page de connexion est dédiée </p>
+        <p>aux responsables départemental</p>
         <h1 class="h3 mb-3 font-weight-normal">Se connecter</h1>
         <?php echo form_open("welcome/connexion"); ?>
-        <label for="inputEmail" class="sr-only">Email address</label>
-
-        <input type="email" id="inputEmail" class="form-control mb-1" name="identifiant" value=""  placeholder="Email address" required autofocus />
-
+        <div class="form-group">
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" id="inputEmail" class="form-control mb-1" name="identifiant" value=""  placeholder="Email address" required autofocus />
+        </div>
+        <div class="form-group">
         <label for="inputPassword" class="sr-only">Password</label>
         <input id="inputPassword" class="form-control mb-1" type="password" name="password" value="" placeholder="Password" required />
-
+        </div>
         <input class="btn btn-lg btn-primary btn-block" type="submit" name="login" value="Connexion">
         </form>
-        
+
     </div>
-    
-        
-        
+
+
+
 
 </div>
