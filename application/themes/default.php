c147bea0d6342d34f51cfcdf7451421ee3544f21
meierdavid <davidmeier@hotmail.fr>
2019-05-18 14:16:11 +0200

    front bootstrap

--- application/themes/default.php
+++ application/themes/default.php
@@ -79,16 +79,18 @@
         </div>
         <?php 
         if($footer == true){ ?>
-        <footer class="page-footer font-small mt-5 mb-0">
-            <div class="text-center ">
-                <a> Nous contacter</a>
-                <p> adresse@email.com </p>
-            </div>
-            <div class="text-center ">
-                <a>Mentions légales:</a>
-                <p>hébergé par la société Heroku, Inc.</p>
-                <p>650 7th Street San Francisco, CA 94103</p>
-                <p>Contact: https://www.heroku.com/contact </p>
+        <footer class="page-footer font-small mt-5 mb-0 container">
+            <div class="row">
+                <div class="text-center col-4 ">
+                    <a> Nous contacter</a>
+                    <p> adresse@email.com </p>
+                </div>
+                <div class="text-center  col-4 ">
+                    <a>Mentions légales:</a>
+                    <p>hébergé par la société Heroku, Inc.</p>
+                    <p>650 7th Street San Francisco, CA 94103</p>
+                    <p>Contact: https://www.heroku.com/contact </p>
+                </div>
             </div>
         </footer>
         <?php } ?>
