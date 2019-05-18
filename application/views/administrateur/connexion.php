bb9b93e76c80ba1500ccffc33ceb38e7c9bb95e5
meierdavid <davidmeier@hotmail.fr>
2019-05-18 15:01:11 +0200

    debug css

--- application/views/administrateur/connexion.php
+++ application/views/administrateur/connexion.php
@@ -1,16 +1,23 @@
-<div class="row mt-5 mb-5 text-center">
-        <p class="text-warning">Cette page de connexion est dédiée </p>
-        <p class="text-warning">aux responsables départemental</p>
+<div class="row justify-content-md-center mt-5 mb-5">
+
+    <div class="text-center">
+        <p>Cette page de connexion est dédiée aux responsables départementaux</p>
+
         <h1 class="h3 mb-3 font-weight-normal">Se connecter</h1>
-        <?php echo form_open("welcome/connexion",'class="ml-5 mr-5 m-auto"'); ?>
-        <div class="form-group">
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control mb-1" name="identifiant" value=""  placeholder="Email address" required autofocus />
-        </div>
-        <div class="form-group">
+        <?php echo form_open("welcome/connexion"); ?>
+        <label for="inputEmail" class="sr-only">Email address</label>
+
+        <input type="email" id="inputEmail" class="form-control mb-1" name="identifiant" value=""  placeholder="Email address" required autofocus />
+
         <label for="inputPassword" class="sr-only">Password</label>
         <input id="inputPassword" class="form-control mb-1" type="password" name="password" value="" placeholder="Password" required />
-        </div>
+
         <input class="btn btn-lg btn-primary btn-block" type="submit" name="login" value="Connexion">
         </form>
+        
+    </div>
+    
+        
+        
+
 </div>
